e164dfaecf9831cde27b4a91c066c05bac6dd3ad
ofektheking123456 <ofek2oo2ooo@gmail.com>
2026-08-25 14:47:53 +0300

    Add fox_tanzanite.mk for Xiaomi Redmi Note 14 4G

---
+++ fox_tanzanite.mk
@@ -0,0 +1,8 @@
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, device/xiaomi/tanzanite/device.mk)
+
+PRODUCT_NAME := fox_tanzanite
+PRODUCT_DEVICE := tanzanite
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi Note 14 4G
+PRODUCT_MANUFACTURER := Xiaomi
