485bdaee3b5aec3710587812496b1f67b74a61b7
ofektheking123456 <ofek2oo2ooo@gmail.com>
2026-08-25 14:58:50 +0300

    Change PRODUCT_NAME from fox_tanzanite to twrp_tanzanite

--- twrp_tanzanite.mk
+++ twrp_tanzanite.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/xiaomi/tanzanite/device.mk)
 
-PRODUCT_NAME := fox_tanzanite
+PRODUCT_NAME := twrp_tanzanite
 PRODUCT_DEVICE := tanzanite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 14 4G
