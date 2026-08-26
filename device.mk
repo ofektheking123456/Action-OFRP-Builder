486204324ce8c5dbc93e6311e0c893802fd27171
ofektheking123456 <ofek2oo2ooo@gmail.com>
2026-08-26 22:07:31 +0300

    Update device.mk for API level and dynamic partitions

--- device.mk
+++ device.mk
@@ -1,2 +1,11 @@
-# Base device configuration
 LOCAL_PATH := device/xiaomi/tanzanite
+
+# API Level
+PRODUCT_SHIPPING_API_LEVEL := 34
+
+# Dynamic Partitions
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Enable Fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
