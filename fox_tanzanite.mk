e8e494a070f53b9761d32ea7890caf17125cc6a7
ofektheking123456 <ofek2oo2ooo@gmail.com>
2026-08-26 22:07:07 +0300

    Update configuration for OrangeFox build

--- fox_tanzanite.mk
+++ fox_tanzanite.mk
@@ -1,8 +1,22 @@
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit OrangeFox configuration
+$(call inherit-product, vendor/fox/config/common.mk)
+
+# Inherit device configuration
 $(call inherit-product, device/xiaomi/tanzanite/device.mk)
 
+# Device identifier
 PRODUCT_NAME := fox_tanzanite
 PRODUCT_DEVICE := tanzanite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 14 4G
 PRODUCT_MANUFACTURER := Xiaomi
+
+# OrangeFox Build Flags
+FOX_VERSION := R11.1
+FOX_BUILD_TYPE := Unofficially
+OF_KEEP_FORCED_ENCRYPTION := 1
+OF_ALLOW_DISABLE_NAVBAR := 0
+OF_USE_MAGISKBOOT := 1
+OF_USE_MAGISKBOOT_FOR_ALL_PATCHES := 1
+OF_DONT_KEEP_LOG_HISTORY := 1
+OF_NO_TREBLE_COMPATIBILITY_CHECK := 1
